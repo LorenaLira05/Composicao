8c9ca1aaa6a4a599f1bc75f8017e68ffcd23d373
Lorena Lira <naheelee2005@gmail.com>
2026-01-26 16:10:12 -0300

    Add product parameter to OrderItem constructor

--- composicao/src/OrderSummary/src/entities/OrderItem.java
+++ composicao/src/OrderSummary/src/entities/OrderItem.java
@@ -10,10 +10,11 @@ public class OrderItem {
 		
 	}
 
-	public OrderItem(Integer quantity, Double price) {
+	public OrderItem(Integer quantity, Double price, Product product) {
 		super();
 		this.quantity = quantity;
 		this.price = price;
+		this.product = product;
 	}
 
 	public Integer getQuantity() {
@@ -36,5 +37,14 @@ public class OrderItem {
 		return price * quantity;
 	}
 	
-	
+	@Override
+	public String toString() {
+		return product.getName() 
+         + ", $" 
+         + String.format("%.2f", price) 
+         + ", Quantity: " 
+         + quantity 
+         + ", Subtotal: $" 
+         + String.format("%.2f", subTotal());
+	}
 }
